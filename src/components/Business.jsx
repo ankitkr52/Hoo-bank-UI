29edec216fedec944c6136963158a32468fa9699
ankitkr52 <ankit72p@gmail.com>
2026-02-10 21:59:49 +0530

    make business section

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-
-const Business = () => {
-  return (
+import { features } from '../constants'
+import styles,{layout} from '../style'
+import Button from './Button'
+const Business = () =>  (
     <div>
       
     </div>
   )
-}
+
 
 export default Business
