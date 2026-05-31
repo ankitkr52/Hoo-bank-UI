062cafdb6b9749d85927948fbaeb22c810c58604
Ankit kumar <ankit72p@gmail.com>
2026-05-31 19:32:48 +0530

    Fix typo in Hero component paragraph

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,7 +28,7 @@ const Hero = () => (
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
         Our team of experts uses a methodology to identify the credit cards
         most likely to fit your needs. We examine annual percentage rates,
-        annual fees.
+        annual fee.
       </p>
     </div>
 
